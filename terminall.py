574533c7f1e09ab9bc7bdb9a07aff3ffafee1cf9
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2020-08-12 23:22:09 +0430

    Create terminall.py

---
+++ terminall.py
@@ -0,0 +1,69 @@
+import os
+print"wellcome T0  terminall M0bIN_dAN"
+os.system("pkg install git")
+os.system("git clone https://github.com/Hax4us/Metasploit_termux.git")
+os.system("cd Metasploit_termux")
+os.system("chmod +x metasploit.sh")
+os.system("bash metasploit.sh")
+os.system("pkg install tor")
+os.system("git clone https://github.com/Mobin-Dan/console_crack_insta")
+os.system("cd console_crack_insta")
+os.system("python2 mobin_dan_console.py")
+os.system("git clone https://github.com/Mobin-Dan/tool-hack")
+os.system("cd tool-hack")
+os.system("python2 tool.py")
+os.system("git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev")
+os.sysem("cd sqlmap-dev")
+os.system("python sqlmap.py -h")
+os.system("git clone https://github.com/Screetsec/microsploit.git")
+os.system("chmod +x Microsploit && ./Microsploit")
+os.system("git clone https://github.com/rajkumardusad/Tool-X.git")
+os.system("cd Tool-X")
+os.system("bash install.sh")
+os.system("Tool-X")
+x=raw_input("$")
+a=os.system("pkg install git")
+a2=os.system("apt-get install git")
+w=os.system("pkg install apdate")
+w2=os.system("apt-get install apdate")
+p=os.system("pkg install php")
+p2=os.system("apt-get install php")
+l=os.system("ls")
+c=os.system("cd")
+s=os.system("sudo")
+s2=os.system("su")
+i=os.system("ping")
+p=os.system("ifconfig")
+d=os.system("pip install")
+cc=os.system("mv")
+bn=os.system("service tor start")
+if x==d:
+    print d
+if x==cc:
+    print cc
+if x==a:
+    print a
+if x==a2:
+    print a2
+if x==w:
+    print w
+if x==w2:
+    print w2
+if x==p:
+    print p
+if x==p2:
+    print p2
+if x==l:
+    print l
+if x==c:
+    print c
+if x==s:
+    print s
+if x==s2:
+    print s2
+if x==i:
+    print i
+ if x==p:
+     print p
+if x==bn:
+    print bn
